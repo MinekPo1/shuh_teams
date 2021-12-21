7c066339692d88fb9381d74902a29f718f45c3b8
MinekPo1 <68391527+MinekPo1@users.noreply.github.com>
2021-12-21 12:20:14 +0100

    Added README Added settings

---
+++ README.md
@@ -0,0 +1,48 @@
+# Shush_teams
+
+This is a simple python script that automatically sets the volume of misbehaving apps to a lower volume in the mixer, if they try to set it high.
+
+Originally written for teams, but can be used for any app.
+
+## Installation
+
+Requirements:
+
+- **python**, written in python 3.10, however, it might work with earlier versions. If you are on windows you can install python 3.6 from the [Microsoft store](https://www.microsoft.com/store/productId/9PJPW5LDXLZ5), if you are on Linux, I assume you know how to install python.
+
+1. Click on the newest release and download the package.zip file.
+2. Extract the contents, and run the shush_teams.py file.
+
+## Settings
+
+The script should generate a settings.yml file in the same directory as the script.
+
+If you are not acquainted with yaml, you can read about it [here](https://www.yaml.org/start.html).
+
+### mode
+
+The mode setting can have one of the following values: `whitelist` or `blacklist`. The whitelist mode will only set the volume of apps whose names that match any regex in [match](#match) or include regex in [include](#include). The blacklist mode will do the opposite.
+
+### match
+
+This setting should be a list of regex expressions. If you don't know what regex is, just writhing the app executable name will most likely work.
+
+### include
+
+Similar to [match](#match), this setting should be a list of regex expressions, however here the expression does not have to match the whole name.
+
+### trigger
+
+Apps which pass the requirements outlined in [mode](#mode), will be checked if their volume is grater or equal to the value of this setting. If it is, the volume will be set to the value of [volume](#volume). If the [mute](#mute) setting is set to `true`, the app will be muted.
+
+### volume
+
+What the app's volume should be set to.
+
+### mute
+
+Whether apps should be muted or not.
+
+### autoupdate
+
+Whether the script should automatically update itself.
--- shush_teams.py
+++ shush_teams.py
@@ -1,9 +1,11 @@
 import io
 import time
-import subprocess
 import sys
 import requests
 import zipfile
+import yaml
+import subprocess
+import re
 
 
 def install(package):
@@ -11,41 +13,133 @@ def install(package):
 
 
 try:
+	import jsonschema
+except ImportError:
+	install("jsonschema")
+	import jsonschema
+try:
 	from pycaw.pycaw import AudioUtilities, AudioSession
 except ImportError:
 	install("pycaw")
 	from pycaw.pycaw import AudioUtilities, AudioSession
 
-# check for updates
-version = "v1.4"
-r = requests.get("https://api.github.com/repos/MinekPo1/shuh_teams/releases")
-j = r.json()
-if r.status_code != 200:
-	print("[!] Error: Could not check for updates")
-elif j[0]["tag_name"] != version:
-	print("[!] Update available: {}".format(j[0]["tag_name"]))
-	print("[!] Downloading update...")
-	r = requests.get(j[0]["assets"][0]["browser_download_url"])
-	if r.status_code != 200:
-		print("[!] Error: Could not download update")
-	else:
-		z = zipfile.ZipFile(io.BytesIO(r.content))
-		for i in z.filelist:
-			if i.filename == "shush_teams.py":
-				d = z.read(i)
-				with open(__file__, "wb") as f:
-					f.write(d)
-				print("[!] Update downloaded")
-				print("[!] Restarting...")
-				subprocess.Popen([sys.executable, __file__])
-				sys.exit()
-	print("[!] Update not downloaded")
 
+SETTINGS_FILE_PATH = __file__.replace(
+	__file__.split("/")[-1].split("\\")[-1], "settings.yml"
+)
+
+settings = {
+		"volume": 0.02,
+		"trigger":1,
+		"mute": False,
+		"mode": "whitelist",
+		"match": [
+			"Teams.exe",
+		],
+		"includes":[],
+		"autoupdate": True,
+	}
+
+# check if file exists, and load it if yes
+try:
+	with open(SETTINGS_FILE_PATH, "r") as f:
+		schema = {
+			"$schema": "http://json-schema.org/draft-07/schema#",
+			"type": "object",
+			"properties": {
+				"volume": {
+					"type": "number",
+					"minimum": 0,
+					"maximum": 1,
+				},
+				"mute": {
+					"type": "boolean",
+				},
+				"match": {
+					"type": "array",
+					"items": {
+						"type": "string",
+					},
+				},
+				"mode": {
+					"type": "string",
+					"enum": ["whitelist", "blacklist"],
+				},
+				"includes": {
+					"type": "array",
+					"items": {
+						"type": "string",
+					},
+				},
+				"trigger": {
+					"type": "number",
+					"minimum": 0,
+					"maximum": 1,
+				},
+				"autoupdate": {
+					"type": "boolean",
+				}
+			},
+		}
+		j = yaml.safe_load(f)
+
+		try:
+			jsonschema.validate(j, schema)
+			settings.update(j)  # allow missing keys
+		except jsonschema.ValidationError as e:
+			print("[!] Error: Invalid settings file")
+			print(e)
+			print("[!] Using default settings")
+
+except FileNotFoundError:
+	print("[!] Settings file not found, creating...")
+	with open(SETTINGS_FILE_PATH, "w") as f:
+		yaml.safe_dump(settings, f)
+
+
+# check for updates
+if settings["autoupdate"]:
+	version = "v1.5"
+	r = requests.get("https://api.github.com/repos/MinekPo1/shuh_teams/releases")
+	j = r.json()
+	if r.status_code != 200:
+		print("[!] Error: Could not check for updates")
+	elif j[0]["tag_name"] > version:
+		print("[!] Update available: {}".format(j[0]["tag_name"]))
+		print("[!] Downloading update...")
+		r = requests.get(j[0]["assets"][0]["browser_download_url"],stream=True)
+		if r.status_code != 200:
+			print("[!] Error: Could not download update")
+		else:
+			s = io.BytesIO()
+			for chunk in r.iter_content(chunk_size=1024):
+				if chunk:
+					s.write(chunk)
+					print(
+						"[!] Downloading: {}%".format(int(s.tell()/s.getbuffer().nbytes*100)),
+						end="\r"
+					)
+			print("[!] Downloading: 100%")
+			z = zipfile.ZipFile(s)
+			for i in z.filelist:
+				if i.filename == "shush_teams.py":
+					d = z.read(i)
+					with open(__file__, "wb") as f:
+						f.write(d)
+					print("[!] Update downloaded")
+					print("[!] Restarting...")
+					subprocess.Popen([sys.executable, __file__])
+					sys.exit()
+		print("[!] Update not downloaded")
+else:
+	print("[!] Autoupdate disabled")
 
 print("Running...\r",end="")
 t = time.time()
 d = 3
 try:
+	p_app = None
+	app_x = 0
 	while True:
 		sessions: list[AudioSession] = AudioUtilities.GetAllSessions()
 
@@ -55,11 +149,30 @@ try:
 			print("Running"+"."*(d)+"   ",end="\r")
 
 		for i in sessions:
-			if i.Process and i.Process.name() == "Teams.exe":
-				vol = i.SimpleAudioVolume
-				if vol.GetMasterVolume() == 1:
-					vol.SetMasterVolume(.02, None)
-					print("Shuh")
+			if i.Process:
+				matches = (
+					any(
+						re.fullmatch(j, i.Process.name()) is not None
+						for j in settings["match"]
+					)
+					or any(
+						re.search(j, i.Process.name()) is not None
+						for j in settings["includes"]
+					)
+				)
+
+				if not(matches ^ (settings["mode"] == "whitelist")):
+					vol = i.SimpleAudioVolume
+					if vol.GetMasterVolume() >= settings["trigger"]:
+						vol.SetMasterVolume(settings["volume"], None)
+						vol.SetMute(settings["mute"], None)
+						if p_app != i.Process.name():
+							print("Shush",i.Process.name())
+							app_x = 1
+							p_app = i.Process.name()
+						else:
+							app_x += 1
+							print("\033[FShush",i.Process.name(),"x",app_x)
 except KeyboardInterrupt:
 	print("\n[!] Shutting down...")
 	sys.exit()
